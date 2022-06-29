3c50fb724786a67347946b09b6002036ed120a15
ٌٍRanaYahya <Ryalnajjar@gmail.com>
2022-06-29 20:16:03 +0300

    logIn and sign up

--- app/src/main/java/com/seu/kfmcapp/LogIn.java
+++ app/src/main/java/com/seu/kfmcapp/LogIn.java
@@ -2,13 +2,27 @@ package com.seu.kfmcapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class LogIn extends AppCompatActivity {
-
+    TextView signUptxt;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_log_in);
+        setSignUptxt();
+    }
+    private void setSignUptxt(){
+    signUptxt= (TextView) findViewById(R.id.signUpPrompttxt);
+    signUptxt.setOnClickListener(new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            Intent intent = new Intent(LogIn.this, Registration.class);
+            startActivity(intent);
+        }
+        });
     }
 }
